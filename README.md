5ad08b0bfda204f398cac278f47bc0f748c590a2
Jacco Flenter <github@putpixel.nl>
2013-11-07 12:08:26 +0100

    extra parameters added

--- README.md
+++ README.md
@@ -1,18 +1,47 @@
 # pip-install
 
-Install the requirements defined in `requirements.txt`. This step will fail if this file could not be found.
+Install the requirements defined in `requirements.txt`. This step will fail if
+this file could not be found.
 
-It will use the wercker cache to speed up future builds.
+It will use the wercker cache to speed up future builds. Wheel support only
+works if the environment variable `PIP_USE_WHEEL` is set to true. The pip wheel
+command also has additional requirement. The latest python box meets those
+requirements when the virtualenv step is also used.
 
 ## Options
-
-* `auto_run_wheel` (optional, default=true). If the `PIP_USE_WHEEL` environment variable is set to
-`true`. The pip install step will also run `pip wheel` before running `pip install`. Settings
-`auto_run_wheel` to false will disable this behavior.
+* `requirements_file` (optional, default=requirements.txt). The
+`requirements.txt` file to use. Set to empty if no requirements file is used.
+* `packages_list` (optional, default=""). List of packages to install (usefull
+for installing packages outside of requirements.txt).
+* `auto_run_wheel` (optional, default=true). If the `PIP_USE_WHEEL` environment
+variable is set to true. The pip install step will also run `pip wheel` before
+running pip install. Settings `auto_run_wheel` to false will disable this
+behavior.
+* `cleanup_wheel_dir` (optional, default=false). If the `PIP_WHEEL_DIR`
+environment variable is set. Settings this property to true, will clenaup the
+wheel dir (before running). This may be needed after updates to the box, or
+when packages are updated without changing version numbers. If the wheel dir is
+specified through a pip.ini this option will fail, only `PIP_WHEEL_DIR` is
+supported.
 
 ## Example
 
+Basic usage:
+```
     - pip-install
+```
+
+To disable running wheel altogether, use:
+```
+    - pip-install:
+        auto_run_wheel: false
+```
+
+Run pip install, but clean up the PIP_WHEEL_DIR before running pip wheel:
+```
+    - pip-install:
+        cleanup_wheel_dir: true
+```
 
 # License
 
--- run.sh
+++ run.sh
@@ -1,61 +1,56 @@
-if [ -f "requirements.txt" ]
-then
-    if [ "$WERCKER_PIP_INSTALL_AUTO_RUN_WHEEL" = "true" ]; then
-        if [ -z "$PIP_USE_WHEEL" ]; then
-            if [ "$PIP_USE_WHEEL" = "true" ]; then
-                if [ -z "$WERCKER_WHEEL_DIR" ]; then
-                    pip wheel -r requirements.txt --wheel-dir=WERCKER_WHEEL_DIR
-                else
-                    pip wheel -r requirements.txt
-                fi
-            fi
-        fi
-    fi
-
-    if [ -z "$PIP_DOWNLOAD_CACHE" ]; then
-        info "Environment variable detected for download cache: $PIP_DOWNLOAD_CACHE"
-    fi
-
-    if [ -z "$PIP_DOWNLOAD_CACHE" ]; then
-        info "Environment variable detected for find links: $PIP_FIND_LINKS"
-    fi
-
-    if [ -z "$VIRTUAL_ENV" ]; then
-        info "virtual environment detected."
-        info "Running pip install: pip install -r requirements.txt"
-        pip install -r requirements.txt
+export REQUIREMENTS_ARGS=""
+if [ ${WERCKER_PIP_INSTALL_REQUIREMENTS_FILE} ]; then
+    if [ -f $WERCKER_PIP_INSTALL_REQUIREMENTS_FILE ]; then
+        export REQUIREMENTS_ARGS="$REQUIREMENTS_ARGS -r $WERCKER_PIP_INSTALL_REQUIREMENTS_FILE"
     else
-        echo "No virtual environment detected. Sudo will be used for pip install"
-        info "Running pip install with sudo"
-        sudo pip install -r requirements.txt
+        fail "no $WERCKER_PIP_INSTALL_REQUIREMENTS_FILE found."
     fi
-
-    if [[ $? -ne 0 ]]
-    then
-        fail 'pip install failed'
-    else
-        success 'pip install succeeded'
-    fi
-else
-    fail 'no requirements.txt found in directory'
 fi
 
-cd $WERCKER_SOURCE_DIR
+if [ ${WERCKER_PIP_INSTALL_PACKAGES_LIST} ]; then
+    REQUIREMENTS_ARGS="$REQUIREMENTS_ARGS $WERCKER_PIP_INSTALL_PACKAGES_LIST"
+fi
 
-if [ -f "requirements.txt" ]
-then
+if [ ${PIP_DOWNLOAD_CACHE} ]; then
+    info "Environment variable detected for download cache: $PIP_DOWNLOAD_CACHE"
+fi
 
-    # mkdir -p $WERCKER_CACHE_DIR/.pip/wheel
+if [ ${PIP_FIND_LINKS} ]; then
+    info "Environment variable detected for find links: $PIP_FIND_LINKS"
+fi
 
-    # debug "pip install with download cache"
-
-
-    if [[ $? -ne 0 ]]
-    then
-        fail 'pip install failed'
-    else
-        success 'pip install succeeded'
+if [ "$WERCKER_PIP_INSTALL_CLEANUP_WHEEL_DIR"]; then
+    if [ ${PIP_WHEEL_DIR} ]; then
+        info "Clearing WHEEL_DIR."
+        rm $PIP_USE_WHEEL/*.whl
+    else:
+        fail "Unable to clear wheel dir. PIP_WHEEL_DIR is not defined"
     fi
+fi
+
+if [ "$WERCKER_PIP_INSTALL_AUTO_RUN_WHEEL" = "true" ]; then
+    info "Auto wheel behavior enabled."
+    if [ ${PIP_USE_WHEEL} ]; then
+        info "Pip use wheel is defined... $PIP_USE_WHEEL"
+        if [ "$PIP_USE_WHEEL" = "true" ]; then
+            info "running pip wheel $REQUIREMENTS_ARGS"
+            pip wheel $REQUIREMENTS_ARGS
+        fi
+    fi
+fi
+
+if [ ${VIRTUAL_ENV} ]; then
+    info "virtual environment detected."
+    info "Running pip install: pip install $REQUIREMENTS_ARGS"
+    pip install $REQUIREMENTS_ARGS
 else
-    fail 'no requirements.txt found in directory'
+    echo "No virtual environment detected. Sudo will be used for pip install"
+    info "Running pip install with sudo"
+    sudo pip install $REQUIREMENTS_ARGS
+fi
+if [[ $? -ne 0 ]]
+then
+    fail "pip install failed"
+else
+    success 'pip install succeeded'
 fi
